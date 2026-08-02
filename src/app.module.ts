23a548d0b2feca7047e7a71c13621d962fcaf52e
tam101315-bit <tam101315@gmail.com>
2026-08-02 15:32:10 +0700

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -47,11 +47,11 @@ import { VnpayModule } from "./modules/vnpay/vnpay.module";
       dbName: process.env.MONGO_DB_NAME,
     }),
     BullModule.forRoot({
-      redis: {
-        host: "localhost",
-        port: 6379,
-      },
-    }),
+  redis: {
+    host: process.env.REDIS_HOST || "localhost",
+    port: parseInt(process.env.REDIS_PORT) || 6379,
+  },
+}),
 
     SharedModule,
     AuthModule,
