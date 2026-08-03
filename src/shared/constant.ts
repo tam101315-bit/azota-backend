1c3160236919db42acf4b1a4c73fe2ac32a4476d
tam101315-bit <tam101315@gmail.com>
2026-08-03 19:00:35 +0700

    Update constant.ts

--- src/shared/constant.ts
+++ src/shared/constant.ts
@@ -12,53 +12,18 @@ export enum Gender {
 
 export enum ExamAssignType {
   ALL = "ALL",
-  CLASS = "CLASS",
   STUDENT = "STUDENT",
+  CLASS = "CLASS",
+}
+
+export enum QuestionType {
+  MULTIQUE_CHOICE = "MULTIQUE_CHOICE",
+  ESSAY = "ESSAY",
+  TRUE_FALSE = "TRUE_FALSE",
+  SHORT_ANSWER = "SHORT_ANSWER",
 }
 
 export enum ExamType {
   TEST = "TEST",
   PRACTICE = "PRACTICE",
-}
-
-export enum ShowResult {
-  NO = "NO",
-  SUBMITTED = "SUBMITTED",
-  ALL_SUBMITTED = "ALL_SUBMITTED",
-}
-
-export enum ShowAnswer {
-  NO = "NO",
-  SUBMITTED = "SUBMITTED",
-  ALL_SUBMITTED = "ALL_SUBMITTED",
-  REACHED_POINT = "REACHED_POINT",
-}
-
-export enum FeeType {
-  FREE = "FREE",
-  TEST = "TEST",
-  EXPLAIN = "EXPLAIN",
-}
-
-export enum QuestionType {
-  MULTIQUE_CHOICE = "MULTIQLE_CHOICE",
-  ESSAY = "ESSAY",
-}
-
-// Notification Type
-export enum NotificationType {
-  Homework = "HOMEWORK",
-  Exam = "EXAM",
-}
-
-export enum ExamNotification {
-  NEW_EXAM = "NEW_EXAM",
-  SUBMIT_EXAM = "SUBMIT_EXAM",
-  MARK_EXAM = "MARK_EXAM",
-}
-
-export enum HomeworkNotification {
-  NEW_HOMEWORK = "NEW_HOMEWORK",
-  SUBMIT_HOMEWORK = "SUBMIT_HOMEWORK",
-  MARK_HOMEWORK = "MARK_HOMEWORK",
 }
